b015db768a3f80f63dba525230a417bddb4d161c
Juliano <fehlipe.contato@gmail.com>
2024-08-25 15:14:59 -0300

    Desafio finalizado POO c# da DIO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,17 +3,25 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string modelo;
+        private string imei;
+        private int memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
+
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
+            Console.WriteLine($"{modelo} iniciado.");
         }
 
         public void ReceberLigacao()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone(numero: "(13)99199-9929", modelo: "Iphone 14 PRO MAX", imei: "354217065812345", memoria: 500);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("------=----------=------");
+
+Nokia nokia = new Nokia(numero: "(14)99421-3231", modelo: "Nokia X-30", imei: "862123040781234", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
